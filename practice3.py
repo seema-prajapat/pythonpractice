183282c3dae24dc3f00c1c135efdb8f73c37cc4f
seema-prajapat <seemajip9636@gmail.com>
2025-07-07 13:49:19 +0530

    Age category checker
    take the users age as input and print a message indicating if they are a chlid ,teenager, adult,or senior

---
+++ practice3.py
@@ -0,0 +1,9 @@
+Age = int(input("Enter your Age:"))
+if Age < 10:
+    print("You Are a chlid.")
+elif Age < 18:
+    print("You Are a Teenager.")
+elif Age < 30:
+    print("You Are a Adult.")
+else:
+    print("You Are a senior.")       
