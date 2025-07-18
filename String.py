288e50de60c01f9f6dfcdcdbf7e432d6e45cfbda
seema-prajapat <seemajip9636@gmail.com>
2025-07-18 14:32:40 +0530

    String.

---
+++ String.py
@@ -0,0 +1,12 @@
+#string= In python string is a sequence of characters enclosed in quotes.
+#types of string:-
+#1.single quote string (' '):-'Hello'
+#2.double quote string (" "):- "Hello"
+#3.triple quote string ("""  """" Or '''  '''):-""" Hello""" Or '''Hello'''
+#triple quote string is use for multiline strings.
+A='seema'
+print(A)
+A="kumari"
+print(A)
+A="""prajapat"""
+print(A)
