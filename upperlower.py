b561e9fe24c35c3791aa6d4bd0a947e5185858b4
seema-prajapat <seemajip9636@gmail.com>
2025-07-18 14:31:56 +0530

    character case checker (uppercase or lowercase).

---
+++ upperlower.py
@@ -0,0 +1,31 @@
+char = input("Enter a single letter:")
+if char.isupper():
+    print("uppercase.")
+elif char.islower():
+    print("lowercase.")       
+else:
+    print("invalid letter.")        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
