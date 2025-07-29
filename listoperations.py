f4a9c2bae549aed093605594084a786ddb542427
seema-prajapat <seemajip9636@gmail.com>
2025-07-29 14:39:45 +0530

    Operations of list..

---
+++ listoperations.py
@@ -0,0 +1,15 @@
+#List Operations:-
+#1. concatenation:-
+list1 = [1,2,3]
+list2 = [4,5]
+list3 =list1+list2
+print(list3)                  #output = [1,2,3,4,5]
+#2.Repeatition:-
+list = [1,2,3]
+Newlist = list*4
+print(Newlist)              #output =[1,2,3,1,2,3,1,2,3,1,2,3]
+#3.Membership:-
+a =[1,3,5,7,9]
+print(5 in a)                    #output = true
+print(2 not in a)                 #output = true
+print(4 in a)                      #output = false
