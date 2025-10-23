47ecb663b50cae30cd09f03c72c6c2f772f3eab7
seema-prajapat <seemajip9636@gmail.com>
2025-10-23 18:37:15 +0530

    number is positive,negative or zero...

---
+++ positivenegativechecker.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number:"))
+if number > 0:
+    print( number," is a positive number")
+elif number == 0:
+    print("zero") 
+else:
+    print(number," is a negative number")       
